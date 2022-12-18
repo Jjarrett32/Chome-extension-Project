8a4358a81c24cc7643d3ad09cacbac697a9a27ef
Jabarre <jjarrett.prsvr@gmail.com>
2022-12-17 20:52:29 -0600

    index.js and index.html

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+console.log(ulEl)
 function saveInput() {
-    console.log("Button clicked")
+    // console.log("Button clicked")
 
 }
+//Line 12 -15 is how the data inside input field is being pused into the myLeads array when the button is clicked
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    renderLeads()
+})
+//line 16-19 is how you do an ordeered list in the html
+for (let i = 0; i < myLeads. length; i++ ) {
+ ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+ //everrthing below is alternate way of writing line 18
+// const li = document.createElement("li")
+// li.textContent = myLeads[i]
+// ulEl.append(li)
+}
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+   console.log(listItems)
+    }
+    ulEl.innerHTML = listItems  
+}
+renderLeads()
